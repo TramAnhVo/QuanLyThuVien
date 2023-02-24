ba4e099463000d5dcd25e641d0e14b544c89a0e9
Trâm Anh <90310489+TramAnhVo@users.noreply.github.com>
2023-02-25 00:27:13 +0700

    Delete QuanLyThuVien directory

--- QuanLyThuVien/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.vttanh.quanlythuvien {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.vttanh.quanlythuvien to javafx.fxml;
-    exports com.vttanh.quanlythuvien;
-}
